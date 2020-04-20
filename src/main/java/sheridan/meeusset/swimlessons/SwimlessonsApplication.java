a617e2565596547ab6c0d3577e495ff9eb1a99c4
Tommas Meeussen <meeusset@shernet.sheridancollege.ca>
2020-04-20 06:17:31 -0400

    init

---
+++ src/main/java/sheridan/meeusset/swimlessons/SwimlessonsApplication.java
@@ -0,0 +1,13 @@
+package sheridan.meeusset.swimlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimlessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwimlessonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/meeusset/swimlessons/SwimlessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.meeusset.swimlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimlessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
